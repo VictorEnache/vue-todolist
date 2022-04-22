faf9fa77a86b8f7e51e8fa6badb6e96a66aa8cad
VictorEnache <victor22enache@gmail.com>
2022-04-22 13:50:00 +0200

    html e css

---
+++ main.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+
+
+        list: [
+
+            {
+                text: 'studiare HTML',
+                done: true
+            },
+            {
+                text: 'studiare CSS',
+                done: false
+            },
+            {
+                text: 'studiare JavaScript',
+                done: true
+            },
+            {
+                text: 'studiare VueJs',
+                done: false
+            }
+        ],
+
+        xmark: '<i class="fa-solid fa-circle-xmark"></i>',
+        
+    },
+
+    methods:{
+        isDone(index){
+            if(this.list[index].done){
+                return 'done'
+            }
+        }
+    }
+
+})
